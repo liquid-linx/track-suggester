f3923240119f663f7fb7a29d7c27eb7a7e52bd4d
os <solano.adi@gmail.com>
2017-12-01 13:31:17 -0800

    added css classes for survey results

--- js/jquery.js
+++ js/jquery.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
   $("form#survey").submit(function(event) {
     event.preventDefault();
     var backFront = $("input:radio[name=1]:checked").val();
-    var backFront = $("input:radio[name=2]:checked").val();
-    var backFront = $("input:radio[name=3]:checked").val();
-    var backFront = $("input:radio[name=4]:checked").val();
-    var backFront = $("input:radio[name=5]:checked").val();
+    var platform = $("input:radio[name=2]:checked").val();
+    var ease = $("input:radio[name=3]:checked").val();
+    var enviro = $("input:radio[name=4]:checked").val();
+    var coin = $("input:radio[name=5]:checked").val();
 
-    else if (johnson === "no" && artist === "yes") {
-        alert("You are Maude");
-        $(".maude").show();
+    if (backfront === "back") {
+        alert("Good Luck!");
+        $(".csharp").show();
 
 
 
