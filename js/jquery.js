17543da5144b1387f610ac54540b69f7413b1390
os <solano.adi@gmail.com>
2017-12-01 12:47:34 -0800

    added variables to survey questions

--- js/jquery.js
+++ js/jquery.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
     event.preventDefault();
+    var backFront = $("input:radio[name=1]:checked").val();
+    var backFront = $("input:radio[name=2]:checked").val();
+    var backFront = $("input:radio[name=3]:checked").val();
+    var backFront = $("input:radio[name=4]:checked").val();
+    var backFront = $("input:radio[name=5]:checked").val();
+
+    else if (johnson === "no" && artist === "yes") {
+        alert("You are Maude");
+        $(".maude").show();
+
+
+
+      });
+    });
