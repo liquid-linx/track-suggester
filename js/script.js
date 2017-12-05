c951de2dda5f935fb08d0bae58025a05f2483d3e
os <solano.adi@gmail.com>
2017-12-05 10:24:15 -0800

    updated variable names, added branching options

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
     event.preventDefault();
-    var backFront = $("input:radio[name=1]:checked").val();
-    var platform = $("input:radio[name=2]:checked").val();
-    var ease = $("input:radio[name=3]:checked").val();
-    var enviro = $("input:radio[name=4]:checked").val();
-    var coin = $("input:radio[name=5]:checked").val();
+    var question1 = $("input:radio[name=1]:checked").val();
+    var question2 = $("input:radio[name=2]:checked").val();
+    var question3 = $("input:radio[name=3]:checked").val();
+    var question4 = $("input:radio[name=4]:checked").val();
+    var question5 = $("input:radio[name=5]:checked").val();
 
-    if (backFront === "back" && platform === "network" && ease === "no" && enviro === "large" && coin === "logic") {
-              alert("Do or do not. There is no try.");
-              $(".csharp").show();
-
-
-      }
-      });
-    });
+    if (question1 === "back" && question2 === "network" && question3 === "no" && question4 === "large" && question5 === "logic") {
+      alert("Do or do not. There is no try.");
+      $(".csharp").show();
+    } else if (question1 = "both" && question3 === "yes" && question4 === "modest" && question5 === "ui") {
+      alert("Do or do not. There is no try.")
+      $(".ruby").show();
+    } else if (question3 === "yes" && question4 === "large" && question5 === "ui") {
+      alert("Do or do not. There is no try.")
+      $(".css").show();
+    } else if (question2 === "mobile" && question3 === "no" && question4 === "large" && question5 === "logic") {
+      alert("Do or do not. There is no try.")
+      $(".android").show();
+    }
+  });
+});
